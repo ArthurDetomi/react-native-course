40d8d14b04978eb3012dc3171e7b3809554a2667
Arthur Detomi <arthurdetomi@gmail.com>
2025-08-07 16:09:42 -0300

    Part 10: Exercise 10.10 completed

--- rate-repository-app/src/themes.js
+++ rate-repository-app/src/themes.js
@@ -1,3 +1,5 @@
+import { Platform } from "react-native";
+
 const theme = {
   colors: {
     textPrimary: "#24292e",
@@ -9,7 +11,12 @@ const theme = {
     subheading: 16,
   },
   fonts: {
-    main: "System",
+    main: Platform.select({
+      ios: "Arial",
+      android: "Roboto",
+      web: "Roboto",
+      default: "System",
+    }),
   },
   fontWeights: {
     normal: "400",
